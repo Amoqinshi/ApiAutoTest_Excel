59c858a2615d3cbf89525ac8006dccc57c757a50
qinshi <qinshi@gaoding.com>
2022-05-04 15:08:29 +0800

    Initial commit

---
+++ run.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：ApiAutoTest 
+@File ：run.py
+@Author ：琴师
+@Date ：2022/5/4 2:59 下午 
+'''
---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：ApiAutoTest 
+@File ：__init__.py.py
+@Author ：琴师
+@Date ：2022/5/4 2:55 下午 
+'''
