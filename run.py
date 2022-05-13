b716a848965d8fd6ab2748f9122295cbc49d531a
qinshi <qinshi@gaoding.com>
2022-05-13 19:30:13 +0800

    更新run.py

--- run.py
+++ run.py
@@ -6,3 +6,11 @@
 @Author ：琴师
 @Date ：2022/5/4 2:59 下午 
 '''
+import os
+import pytest
+from config import Conf
+
+if __name__=="__main__":
+    report_path = Conf.get_report_path() + os.sep + "result"
+    report_html_path = Conf.get_report_path() + os.sep + "html"
+    pytest.main(["-s","--alluredir",report_path])
